51565648dd73833e201d3845bf3f32f1b3cbd3d6
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-04-10 14:43:33 +0300

    Added price field to Product model
    Signed-off-by: Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>

--- shop/models.py
+++ shop/models.py
@@ -3,5 +3,9 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name + " - $" + str(self.price)
 
 # TODO: impement file deletion on model delete
